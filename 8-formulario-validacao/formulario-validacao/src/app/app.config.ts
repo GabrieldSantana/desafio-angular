f6c74f2e9d80de96a04b9971f525cfeefcd19342
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-11 18:49:30 -0300

    feat: biblioteca de máscara adicionada

---
+++ 8-formulario-validacao/formulario-validacao/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideEnvironmentNgxMask, provideNgxMask } from 'ngx-mask';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideEnvironmentNgxMask(),
+    provideNgxMask()
+  ]
+};
